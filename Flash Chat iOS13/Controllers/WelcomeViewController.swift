eecc436e7c20051c021327bbda8eef03001863be
jd <jhojan_54_56@hotmail.com>
2023-11-03 17:11:36 -0500

    Implement CLTypingLabel from Cocoapods

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,26 +9,18 @@
 
 import UIKit
 import LocalAuthentication
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(WelcomeViewController.authenticationCompletionHandler(loginStatusNotification:)), name: .MTBiometricAuthenticationNotificationLoginStatus, object: nil)
         
-        titleLabel.text=""
-        var charIndex = 0.0
-        let titleText = "OnlyChat"
-        
-        for letter in titleText{
-       
-            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false){
-                (timer) in self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        titleLabel.text="OnlyChat"
+      
         
     }
     
