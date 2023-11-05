c563a4ce60bfee468453556a06b63812a8df1dd5
jd <jhojan_54_56@hotmail.com>
2023-11-05 08:09:49 -0500

    keyboard config from cocapods

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -19,7 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let db = Firestore.firestore()
         
         print(db)
-
+        IQKeyboardManager.shared.enable=true
+             IQKeyboardManager.shared.enableAutoToolbar = false
+             IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         return true
     }
 
